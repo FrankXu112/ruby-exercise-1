ae68407b04aefdb48b16c46024d5e1d6f3324faa
Frank Xu <frankxu@Franks-MacBook-Pro-2.local>
2015-03-31 14:58:58 -0400

    fix exercise2

--- exercise2.rb
+++ exercise2.rb
@@ -1,1 +1,9 @@
-puts 55 * 1.2
+puts 55 * 0.2
+# problem 1
+puts 2.to_s, "hello"
+# problem 2
+num = 45628 * 7839
+puts "The result is #{num}."
+# problem 3
+puts (true && false) || (false && true) || !(false && false)
+# problem 4
